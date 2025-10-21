72e70dcae7554b53c8dad6c26dc74c0f2faf0bed
MoYuM <64450989+MoYuM@users.noreply.github.com>
2025-10-21 09:31:28 +0000

    add fish config

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    starship init fish | source
+end
+
+alias ls "ls -p -G"
+alias la "ls -A"
+alias ll "ls -l"
+
+fish_add_path $HOME/.local/bin
+fish_add_path /opt/homebrew/bin
