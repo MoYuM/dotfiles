fa2cf355d6caf38461e9cb3b5045827977d658e0
MoYuM <64450989+MoYuM@users.noreply.github.com>
2026-04-24 03:49:11 +0000

    feat: update

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,8 +5,20 @@ end
 
 alias lg "lazygit"
 alias ca "cursor-agent"
+alias cc "claude --dangerously-skip-permissions"
+alias cz "chezmoi"
 
 set -gx XDG_CONFIG_HOME $HOME/.config
 
 fish_add_path $HOME/.local/bin
 fish_add_path /opt/homebrew/bin
+
+# pnpm
+set -gx PNPM_HOME "/Users/moyum/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+export EDITOR="nvim"
+
